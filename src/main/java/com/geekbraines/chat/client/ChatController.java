97b3ea1d4a4aa9b24a6eafc317b7d4c65a040312
IgorLevch <igorlevvv@gmail.com>
2023-02-19 23:44:50 +0300

    The second commit

---
+++ src/main/java/com/geekbraines/chat/client/ChatController.java
@@ -0,0 +1,38 @@
+package com.geekbraines.chat.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TitledPane;
+
+public class ChatController {
+
+    @FXML
+    public ListView <String> field_with_files;
+    @FXML
+    public TitledPane title_pane;
+
+
+
+
+
+    public void getout(ActionEvent actionEvent) {
+    }
+
+
+    public void delete(ActionEvent actionEvent) {
+    }
+
+    public void add_file(ActionEvent actionEvent) {
+    }
+
+    public void unload_file(ActionEvent actionEvent) {
+    }
+
+    public void share_file(ActionEvent actionEvent) {
+    }
+
+    public void rename_file(ActionEvent actionEvent) {
+    }
+}
